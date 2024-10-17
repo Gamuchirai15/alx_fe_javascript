77fecfc5764a89347e4c6ded429a225a5f6c5eda
Gamuchirai15 <gamiechif@gmail.com>
2024-10-17 09:38:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -45,8 +45,28 @@ function addQuote() {
     } else {
         alert("Please enter both quote and category.");
     }
-}
 
-createAddQuoteForm();
+    function createAddQuoteForm() {
+        const formContainer = document.getElementById("newQuoteForm");
+        const quoteInput = document.createElement("input");
+        quoteInput.id = "newQuoteText";
+        quoteInput.type = "text";
+        quoteInput.placeholder = "Enter a new quote";
+    
+        const categoryInput = document.createElement("input");
+        categoryInput.id = "newQuoteCategory";
+        categoryInput.type = "text";
+        categoryInput.placeholder = "Enter quote category";
+    
+        const addButton = document.createElement("button");
+        addButton.innerText = "Add Quote";
+        addButton.onclick = addQuote;
+        formContainer.appendChild(quoteInput);
+        formContainer.appendChild(categoryInput);
+        formContainer.appendChild(addButton);
+    }
+
+    createAddQuoteForm();
+}
 
 document.getElementById("newQuote").addEventListener("click", showRandomQuote);
